1e7954b1676f65254d0a8dba7e26aead1ee1df64
Komalm3131 <komalmirje2606@gmail.com>
2025-03-04 21:02:20 +0530

    home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,21 +8,21 @@ let Home = () => {
             <h1 class="display-4 fw-bold">Welcome to APNA Bank</h1>
             <p class="lead">Your Trusted Partner in Financial Growth</p>
         </div>
-        <h2>Our Services</h2>
+        <h2>Our Services ...</h2>
         <div class="service-card">
-            <img src="./1img.jpg" alt="Digital Banking">
+           <img src="./1img.jpg" alt="Digital Banking"></img>
             <h3>Digital Banking</h3>
             <p>Seamless online and mobile banking experiences</p>
         </div>
 
         <div class="service-card">
-            <img src="./2.jpgnpm start" alt="Investment Platforms">
+            <img src="./2.jpgnpm start" alt="Investment Platforms"></img>
             <h3>Investment Platforms</h3>
             <p>Smart investment options for every financial goal</p>
         </div>
 
         <div class="service-card">
-            <img src="./3.jpg" alt="Global Transactions">
+            <img src="./3.jpg" alt="Global Transactions"></img>
             <h3>Global Transactions</h3>
             <p>Worldwide banking with low international fees</p>
         </div>
