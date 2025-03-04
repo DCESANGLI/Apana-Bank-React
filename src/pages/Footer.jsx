bfc7412ce48b2288a75e7ad3cc4be837d4fe48f9
shrenik <deesha.computer.education@gmail.com>
2025-03-04 21:01:15 +0530

    merging changes

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,11 +1,32 @@
-
-
 let Footer = () => {
-    return (
-        <>
-        
-        </>
-    )
-}
+  return (
+    <>
+      <footer class="bg-primary text-white text-center py-3 mt-4 bg-opacity-75">
+        <div class="container mt-4">
+          <div class="row text-center">
+            <div class="col-md-4" onclick="handleClick('Home')">
+              <i class="fa-solid fa-house"></i>
+              <a href="">
+                <h4 style={{"color": "white"}}>Home</h4>
+              </a>
+            </div>
+            <div class="col-md-4" onclick="handleClick('History')">
+              <i class="fa-solid fa-clock-rotate-left"></i>
+              <a href="">
+                <h4 style={{"color": "white"}}>History</h4>
+              </a>
+            </div>
+            <div class="col-md-4" onclick="handleClick('User')">
+              <i class="fa-solid fa-user"></i>
+              <a href="">
+                <h4 style={{"color": "white"}}>User</h4>
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
 export default Footer;
