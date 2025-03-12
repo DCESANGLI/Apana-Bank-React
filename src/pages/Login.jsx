aa38ca8b0bc7c9104e4cb7a0c43bec08013e6237
shrenik <deesha.computer.education@gmail.com>
2025-03-12 20:44:38 +0530

    login api implemented

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import "./user.style.css";
+import axios from "axios";
 
 let Login = () => {
+  let [data, setData] = useState({email:"", password:""});
+
+  let readInput = (event) => {
+    const {name, value} = event.target;
+
+    setData({...data, [name]:value});
+  }
+
+  let login_click = async() => {
+      await axios.post('http://localhost:4949/api/user/login', data)
+
+      .then(res=>{
+        alert("Login Successfull");
+      })
+
+      .catch(err=>{
+        console.log(JSON.stringify(err.response));
+        alert("Server Side Error");
+      })
+  }
+
   return (
     <>
       <div className="d-flex justify-content-center">
         <div class="container1 my-3">
           <h2>Bank Account Login</h2>
-          <form id="registrationForm">
+      
             
 
             <div class="form-group">
               <label>Email Address</label>
-              <input type="email" id="email" placeholder="Enter email" />
+              <input type="email" name="email" placeholder="Enter email" onChange={readInput}/>
               <div class="error" id="emailError"></div>
             </div>
 
@@ -20,16 +43,17 @@ let Login = () => {
               <label>Password</label>
               <input
                 type="password"
-                id="password"
+                name="password"
                 placeholder="Create a password"
+                onChange={readInput}
               />
               <div class="error" id="passwordError"></div>
             </div>
 
-            <button class="button1" type="submit">
+            <button class="button1" onClick={()=>login_click()}>
               Login
             </button>
-          </form>
+         
         </div>
       </div>
     </>
